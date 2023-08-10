f319db9d7eab757ff09e1f083b68ecef14463605
radame23 <141948584+radame23@users.noreply.github.com>
2023-08-10 10:41:49 -0600

    Add files via upload

---
+++ holamundo3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (int argc,char **argv)
+{
+	printf("*******************************************************************************************************************************************************************\n ");
+    printf("*                                                                                        GENERACION DE                                                            *\n");
+    printf("*                                                                                         COMPUTADORAS                                                            *\n");
+    printf("*******************************************************************************************************************************************************************\n");
+    printf(" \n CUANTAS GENEACIONES DE COMPUTADORAS HAY?                        CARACTERISTICAS\n \n");
+   	printf("*******************************   1.Tecnología de Válvulas de Vacío: Las computadoras de esta generación utilizaban válvulas de vacío para el procesamiento de informacion,\n" );
+    printf("*      PRIMERA GENERACION     *   2.Tamaño y Consumo de Energía: Eran enormes y ocupaban salas enteras\n");
+    printf("*      (1940s-1950s):         *   3.Programación en Lenguaje de Máquina: La programación se realizaba en lenguaje de máquina, \n");
+    printf("*******************************\n \n");
+    printf("*******************************   1.Transistores: Las válvulas de vacío fueron reemplazadas por transistores\n");
+    printf("*      SEGUNDA GENERACION     *   2.Memoria de Núcleos Magnéticos: Se utilizaban memorias de núcleos magnéticos \n");
+    printf("*      (1950s-1960s):         *   3.Lenguajes de Programación de Alto Nivel: Se desarrollaron lenguajes de programación de alto nivel como Fortran y COBOL\n");
+    printf("*******************************\n \n");
+    printf("*******************************   1.Circuitos Integrados: Los circuitos integrados permitieron empaquetar múltiples componentes electrónicos en un solo chip\n");
+    printf("*      TERCERA GENERACION     *   2.Tiempo Compartido: Surgió el concepto de tiempo compartido, que permitía a múltiples usuarios acceder a la computadora simultáneamente, \n");
+    printf("*      (1960s-1970s):         *   3.Sistemas Operativos: Se desarrollaron sistemas operativos más avanzados para gestionar recursos y tareas\n");
+    printf("*******************************\n \n");
+    printf("*******************************   1.Microprocesadores: Se introdujeron los microprocesadores, que eran chips completos de CPU en un solo componente\n");
+    printf("*      CUARTA GENERACION      *   2.Computadoras Personales (PCs): Surgieron las computadoras personales, como la IBM PC, que llevaron la computación a los hogares y las empresas.\n");
+    printf("*      (1970s-1980s):         *   3.Interfaces Gráficas y Ratón: Las interfaces gráficas de usuario (GUI) y el uso del ratón se volvieron comunes\n");
+    printf("*******************************\n \n");
+    printf("*******************************   1.Computación Paralela y Supercomputadoras: Se enfocó en la computación paralela y distribuida, lo que permitió realizar cálculos complejos \n");
+    printf("*      QUINTA GENERACION      *   2.Inteligencia Artificial (IA) y Procesamiento Natural del Lenguaje (NLP): Se desarrollaron sistemas de IA más avanzados\n");
+    printf("*      (1980s-Presente):      *   3.Tecnologías Móviles y Conectividad Global: Se produjo la explosión de dispositivos móviles y la conectividad global a través de internet\n");
+    printf("*******************************\n \n");
+	return 0;
+}
