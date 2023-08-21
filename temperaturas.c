d0d11b1ca7b968d38eeda0066e0fd43d1cf3803a
radame23 <141948584+radame23@users.noreply.github.com>
2023-08-21 17:11:49 -0600

    Add files via upload

--- temperaturas.c
+++ temperaturas.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int main(){
-	int a=10, b=20,c,f,temp;
 	
-	printf("valor de temperatura en celcius %d \n", a);
-	c=(a*9/5)+32;
-	printf("valor de temperatura en fahrenheit %d \n", b);
-	f=(b-32)*(5/9);
 	
-		printf("celcius a fahrenheit %d \n", c);
-		printf("fahrenheit a celcius %d \n", f);
-		
+	float a, b ,c,f,temp;
+	int m=1;
+	printf("CALCULADORA DE TEMPERATURAS \n");
+
+
+
+	printf("valor de temperatura en celcius: \n");
+	scanf("%f", &a);
+	printf("valor de temperatura en fahrenheit \n");
+	scanf("%f", &b);
+	
+		c=(a*9/5)+32;
+		f=(b-32)*5/9;
+		printf("celcius a fahrenheit %f \n", c);
+		printf("fahrenheit a celcius %f \n", f);
+
 		
 	return 0;
 }
